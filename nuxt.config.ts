e1f9b50e691ac6b7f8ef8271d6f60c6c96d0126b
Ali Arshadi <realgabral@protonmail.com>
2025-04-22 14:13:09 +0430

    project colors added to assets nuxt.config.ts updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/colors.scss" as *;',
+        },
+      },
+    },
+  },
 });
