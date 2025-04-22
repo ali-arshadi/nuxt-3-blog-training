307cadc193129c8fecbfed65abc5e7dbb98c6a11
Ali Arshadi <realgabral@protonmail.com>
2025-04-22 10:06:06 +0430

    scss main file added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/scss/main.scss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts']
-})
+  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts'],
+});
