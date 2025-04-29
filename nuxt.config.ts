7d1be147220531fcc97c9c67e2cfe8cf7fa31db4
Ali Arshadi <realgabral@protonmail.com>
2025-04-29 10:27:41 +0430

    json webserver added and run on port 5000 db.json file created
    nuxt.config.ts devProxy and routeRules added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   css: ['~/assets/scss/main.scss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts'],
+  nitro: {
+    devProxy: {
+      '/api': 'http://localhost:5000/api',
+    },
+  },
+  routeRules: {
+    '/api/**': { proxy: 'http://localhost:5000/api/**' },
+  },
   vite: {
     css: {
       preprocessorOptions: {
